2dd46b66f902828ef43fc67d986ed19ddb222fde
mohd-sadab-au3 <sadabkhan14198@gmail.com>
2019-10-30 18:03:43 +0530

    Updates

---
+++ precache-manifest.f2f6ffbfa950306f3a99b7127b307347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb473c6782151f634417770fbe62641",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "d23c14de7990428115bd",
+    "url": "/Contact-Manager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "85353c5dbddc61985e8c",
+    "url": "/Contact-Manager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d23c14de7990428115bd",
+    "url": "/Contact-Manager/static/js/2.aed0b4f1.chunk.js"
+  },
+  {
+    "revision": "85353c5dbddc61985e8c",
+    "url": "/Contact-Manager/static/js/main.2676c5ea.chunk.js"
+  },
+  {
+    "revision": "8ce5d41f594ccccde115",
+    "url": "/Contact-Manager/static/js/runtime-main.f183a4c6.js"
+  }
+]);
